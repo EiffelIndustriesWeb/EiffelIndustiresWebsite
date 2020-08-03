cbebe933461130a6aa5ba61b9fd1c4eaaade7244
future-rob <rob@dark-labs.co>
2020-08-03 12:23:56 -0700

    mobile links

--- src/components/header/navMobile.js
+++ src/components/header/navMobile.js
@@ -23,9 +23,9 @@ export default function NavMobile() {
                                 child0="Structural Engineers"
                                 link0="/structuralEngineers"
                                 child1="Construction"
-                                link1="/about"
+                                link1="/construction"
                                 child2="Better Build"
-                                link2="https://dunamispacific.com/"
+                                link2="/betterBuild"
                             />
                         </li>
                         <li className="nav-item--parent">
