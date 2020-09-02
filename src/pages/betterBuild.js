d17b064d3bc2f7e3a32ccb21b321577669f58a08
future-rob <rob@dark-labs.co>
2020-09-01 17:58:47 -0700

    URGENT changes push

--- src/pages/betterBuild.js
+++ src/pages/betterBuild.js
@@ -23,8 +23,9 @@ const BetterBuildPage = () => (
             <div className="link-title--container">
                 <Link className="page-title--link" to="https://dunamispacific.com/products">Visit Better Build</Link>
             </div>
+            <p className="page--text">Better Build is a premier software solution owned by Dunamis Pacific Corporation, an Eiffel Industries Enterprise.</p>
             <p className="page--text">Dunamis Pacific Corp is a professional IT strategy and enterprise solutions firm where highly experienced IT professionals collaborate together to create and deliver innovative, transparent and agile solutions geared towards increasing our clients’ efficiency, market share and bottom line.</p>
-            <p className="page--text">Eco system for the general contractors and sub-contractors within the North America construction industry, integrating the complete supply chain from project bids to procurement and project delivery to payments.</p>
+            <p className="page--text">Better Build intends to serve as an ecosystem for the general contractors and sub-contractors within the North America construction industry, integrating the complete supply chain from project bids to procurement and project delivery to payments.</p>
             <ImageBetterBuildTeam />
         </div>
         {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
